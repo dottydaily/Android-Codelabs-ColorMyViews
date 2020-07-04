1f33bdec6787b451cb0763f7fa65e524630c5491
Pornpat Santibuppakul <dottypurkt@gmail.com>
2020-07-04 18:34:30 +0700

    Add Section 02.3: Constraint layout using the Layout Editor (Part10-18)

--- app/src/main/java/com/codelabs/colormyviews/MainActivity.kt
+++ app/src/main/java/com/codelabs/colormyviews/MainActivity.kt
@@ -28,13 +28,16 @@ class MainActivity : AppCompatActivity() {
             R.id.box_three_text -> view.setBackgroundResource(R.drawable.box_three_image)
             R.id.box_four_text -> view.setBackgroundResource(R.drawable.box_four_image)
             R.id.box_five_text -> view.setBackgroundResource(R.drawable.box_five_image)
+            R.id.red_button -> view.setBackgroundResource(R.color.my_red)
+            R.id.green_button -> view.setBackgroundResource(R.color.my_green)
+            R.id.yellow_button -> view.setBackgroundResource(R.color.my_yellow)
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
     }
 
     private fun setListeners() {
         val clickableViews: List<View> = listOf(box_one_text, box_two_text, box_three_text,
-            box_four_text, box_five_text, constraint_layout)
+            box_four_text, box_five_text, constraint_layout, red_button, green_button, yellow_button)
 
         clickableViews.forEach { item -> item.setOnClickListener { makeColored(it) } }
     }
